e5f65a3b00b435385ee5416c3677631098465b98
Neil Greatorex <neil@fatboyfat.co.uk>
2014-03-14 23:37:46 +0000

    Add license / copyright to UDP files

--- udp.c
+++ udp.c
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2014 Neil Greatorex (https://github.com/ngreatorex)
+ *
+ *
+ *   This code is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2
+ *   published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -40,8 +59,8 @@ static void setup_networking(char *host, char *port)
 	int s;
 
 	if (verbose) fprintf(stderr, "Sending UDP packets to '%s' port %s\n", host, port);
-	/* Obtain address(es) matching host/port */
-
+	
+	/* Obtain address(es) matching host/port (ripped from man page for getaddrinfo!) */
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
 	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
@@ -53,11 +72,6 @@ static void setup_networking(char *host, char *port)
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
-
-	/* getaddrinfo() returns a list of address structures.
-	   Try each address until we successfully connect(2).
-	   If socket(2) (or connect(2)) fails, we (close the socket
-	   and) try the next address. */
 
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
 		socket_fd = socket(rp->ai_family, rp->ai_socktype,
@@ -82,17 +96,17 @@ static void setup_networking(char *host, char *port)
 void send_udp(acarsmsg_t *msg, int chn, time_t t)
 {
 	char buf[UDP_BUF_SIZE];
-	int size;	
-	
+	int size;
+
 	if (!configured) {
 		fprintf(stderr, "UDP not configured - missing -u option?\n");
 		exit(1);
 	}
-	
-	size = snprintf(buf, UDP_BUF_SIZE-1, "AC%c %7s %c %2s %c %4s %6s %s", 
+
+	size = snprintf(buf, UDP_BUF_SIZE-1, "AC%c %7s %c %2s %c %4s %6s %s",
 		msg->mode,
 		msg->addr,
-		msg->ack == 21 ? '!' : msg->ack, /* Convert NAK to '!' */ 
+		msg->ack == 21 ? '!' : msg->ack, /* Convert NAK to '!' */
 		msg->label,
 		msg->bid,
 		msg->no,
@@ -102,5 +116,5 @@ void send_udp(acarsmsg_t *msg, int chn, time_t t)
 	buf[UDP_BUF_SIZE-1] = 0;
 
 	send(socket_fd, buf, size, 0);
-	printf("UDP -> \"%s\"\n", buf);
+	printf("#%d: UDP packet sent -> \"%s\"\n", chn, buf);
 }
--- udp.h
+++ udp.h
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2014 Neil Greatorex (https://github.com/ngreatorex)
+ *
+ *
+ *   This code is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2
+ *   published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #include <stdio.h>
 
 #include "acars.h"
